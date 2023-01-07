73003df44a36c09b073c07b3084bb0f12f54b65d
xiaorui3 <117619377+xiaorui3@users.noreply.github.com>
2023-01-07 21:16:23 +0800

    Update LinkedList类.java

--- 集合解释/List类/LinkedList类.java
+++ 集合解释/List类/LinkedList类.java
@@ -1,4 +1,14 @@
 package 代码解释.集合解释.List类;
 
+import java.util.LinkedList;
+
 public class LinkedList类 {
+    LinkedList<Integer> link=new LinkedList<>();
+/*    方法名称	说明
+    void addFirst(E e)	将指定元素添加到此集合的开头
+    void addLast(E e)	将指定元素添加到此集合的末尾
+    E getFirst()	返回此集合的第一个元素
+    E getLast()	返回此集合的最后一个元素
+    E removeFirst()	删除此集合中的第一个元素
+    E removeLast()	删除此集合中的最后一个元素*/
 }
