913f998f44cb3b50e7caf5281c8e9a72ded9e043
xiaorui3 <117619377+xiaorui3@users.noreply.github.com>
2023-01-07 21:47:50 +0800

    Create 方法

--- 集合解释/List类/二者对比.java
+++ 集合解释/List类/二者对比.java
@@ -1,5 +1,7 @@
 package 代码解释.集合解释.List类;
 
+import java.util.LinkedList;
+
 public class 二者对比 {
 /*    ArrayList 与 LinkedList 都是 List 接口的实现类，因此都实现了 List 的所有未实现的方法，只是实现的方式有所不同。
 
@@ -8,4 +10,6 @@ public class 二者对比 {
     对于快速访问对象的需求，使用 ArrayList 实现执行效率上会比较好。需要频繁向集合中插入和删除元素时，使用 LinkedList 类比 ArrayList 类效果高。
 
     不同的结构对应于不同的算法，有的考虑节省占用空间，有的考虑提高运行效率，对于程序员而言，它们就像是“熊掌”和“鱼肉”，不可兼得。高运行速度往往是以牺牲空间为代价的，而节省占用空间往往是以牺牲运行速度为代价的。*/
+
 }
+
